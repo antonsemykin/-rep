76e0655806277050e7188ed1b499e2fce6068ffc
antonsemykin <159423366+antonsemykin@users.noreply.github.com>
2024-09-01 15:09:22 +0300

    Create test.py

---
+++ test.py
@@ -0,0 +1,19 @@
+import unittest
+
+from my_sum import sum
+from fractions import Fraction
+
+class TestSum(unittest.TestCase):
+    def test_list_int(self):
+        """
+        Test that it can sum a list of integers
+        """
+        data = [1, 2, 3]
+        result = sum(data)
+        self.assertEqual(result, 6)
+
+        self.assertEqual(result+1, 7)
+        self.assertEqual(result*3, 18)
+
+if __name__ == '__main__':
+    unittest.main()
