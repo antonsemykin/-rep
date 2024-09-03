45a14585b350b246db8f461cc7eab8fbf28bfaf7
antonsemykin <semykin_toxa@mail.ru>
2024-09-04 00:22:12 +0300

    update main.py

--- app/main.py
+++ app/main.py
@@ -1,6 +1,3 @@
-#!/usr/bin/env python3
-#-*- coding: utf-8 -*-
-
 # Very simple tetris implementation
 #
 # Control keys:
@@ -10,31 +7,7 @@
 #     Escape - Quit game
 #          P - Pause game
 #     Return - Instant drop
-#
-# Have fun!
 
-# NOTE: If you're looking for the old python2 version, see
-#       <https://gist.github.com/silvasur/565419/45a3ded61b993d1dd195a8a8688e7dc196b08de8>
-
-# Copyright (c) 2010 "Laria Carolin Chabowski"<me@laria.me>
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-# THE SOFTWARE.
 
 from random import randrange as rand
 import pygame, sys
