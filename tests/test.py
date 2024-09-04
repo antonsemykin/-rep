8616c3d2fe17af320eae364c0a95778a9ef68938
antonsemykin <semykin_toxa@mail.ru>
2024-09-04 21:45:46 +0300

    test3

--- tests/test.py
+++ tests/test.py
@@ -1,15 +1,8 @@
-import unittest
+import pytest
 
-class TestSum(unittest.TestCase):
-    def test_list_int(self):
-        """
-        Test that it can sum a list of integers
-        """
-        data = [1, 2, 3]
-        result = sum(data)
-        self.assertEqual(result, 6)
-        self.assertEqual(result+1, 7)
-        self.assertEqual(result*3, 18)
+def inc(x):
+    return x + 1
 
-# if __name__ == '__main__':
-#     unittest.main()
+
+def test_answer():
+    assert inc(3) == 5
